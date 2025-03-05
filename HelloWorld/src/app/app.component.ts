9fba2b2a952104f937c0eeeb5fd45ff8beb7f9f5
Nishant5454 <bhagatnishant5454@gmail.com>
2025-03-05 17:14:42 +0530

    [Nishant]Add.Event Binding Property

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -4,9 +4,21 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  // template: `<h2>Saurabh</h2>`,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'HelloWorld';
+  imgSrc = 'favicon.ico';//property binding
+  title = 'ANGULAR STARTER';
+  url = 'https://www.bridgelabz.com';
+
+  ngOnInit() {
+    this.title = 'ANGULAR STARTER By Saurabh';
+  }
+
+  onClick() {
+    console.log('Button Clicked');
+    window.open(this.url, '_blank');
+  }
 }
