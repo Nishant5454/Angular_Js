915abc7e62cea67a101b83487a49135801099a3e
Nishant5454 <bhagatnishant5454@gmail.com>
2025-03-05 14:37:24 +0530

    [Nishant]Add.Logo in the HelloWorld Application

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'HelloWorld';
+  imgUrl= 'image.jpg';
   ngOnInit(): void {
     this.title = 'HelloWorld From BridgeLabz';// Done using Binding
 }
